fff4fdd9d81ba4ea10c88b23b66dfad3fa4c64b4
Maphoto EM <92788343+Nemo-DE-JOAT@users.noreply.github.com>
2025-05-21 17:23:48 +0200

    Update StudentFacade.java for Roles Mapping
    Commit Roles Mapping with the @RolesAllowed Annotation

--- ClassListManagerEJBModule/src/java/za/ac/tut/bl/StudentFacade.java
+++ ClassListManagerEJBModule/src/java/za/ac/tut/bl/StudentFacade.java
@@ -7,6 +7,7 @@ package za.ac.tut.bl;
 
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import za.ac.tut.entities.Student;
@@ -30,21 +31,25 @@ public class StudentFacade extends AbstractFacade<Student> implements StudentFac
         super(Student.class);
     }
 
+    @RolesAllowed("teacher")
     @Override
     public void createStudent(Student student) {
         create(student);
     }
 
+    @RolesAllowed("teacher")
     @Override
     public void editStudent(Student student) {
         edit(student);
     }
 
+    @RolesAllowed("teacher")
     @Override
     public void removeStudent(Student student) {
         remove(student);
     }
 
+    @RolesAllowed("teacher")
     @Override
     public Student findStudent(Object id) {
         //find student
@@ -52,6 +57,7 @@ public class StudentFacade extends AbstractFacade<Student> implements StudentFac
         return targetStudent;
     }
 
+    @RolesAllowed("teacher","student")
     @Override
     public List<Student> findAllStudent() {
         //get the students
